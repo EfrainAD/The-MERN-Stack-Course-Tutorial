440dc6fc2253c6e7018236271d940627874c7a46
EfrainAD <105945226+EfrainAD@users.noreply.github.com>
2022-12-24 11:13:16 -0500

    Now Post to the DB

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,28 @@
 const dotenv = require('dotenv').config()
 const PORT = process.env.PORT || 8000
 const MONGO_URI = process.env.MONGO_URI
+
 const express = require('express')
 const mongoose = require('mongoose')
 
+const Task = require('./model/taskModel')
+
 const app = express()
+
+// Middleware
+app.use(express.json())
 
 app.get('/', (req, res) => {
      res.send('<h1>This server is running</h1>')
 })
 
-app.post('/api/tasks', (req, res) => {
-     console.log('req.body', req.body)
-     res.send('Task Created')
+app.post('/api/tasks', async (req, res) => {
+     try {
+          const task = await Task.create(req.body)
+          res.status(200).json(task)
+     } catch (error) {
+          res.status(500).json({message: error.message})
+     }
 })
 
 mongoose.connect(MONGO_URI)
