bea2fbac71c0d086d3cd402521665dcb98e697bd
EfrainAD <105945226+EfrainAD@users.noreply.github.com>
2022-12-25 16:23:33 -0500

    Added cors

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const dotenv = require('dotenv').config()
+const cors = require('cors')
 const PORT = process.env.PORT || 8000
 const MONGO_URI = process.env.MONGO_URI
 
@@ -12,6 +13,9 @@ const app = express()
 
 // Middleware
 app.use(express.json())
+app.use(cors({
+     origin: ['http://localhost:3000']
+}))
 app.use('/api/tasks',taskRouter)
 
 app.get('/', (req, res) => {
