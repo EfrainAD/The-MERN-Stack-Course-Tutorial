7f849b83cc60fa919bdbbf1f5c1fd7d376d1aa3d
EfrainAD <105945226+EfrainAD@users.noreply.github.com>
2022-12-25 15:14:01 -0500

    Filled in the components

--- frontend/src/components/task-form.js
+++ frontend/src/components/task-form.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
-const TaskForm = () => {
+const TaskForm = ({createTask, name, handleInputChange}) => {
      return (
-          <div>
-               <h2>TaskForm</h2>
-          </div>
+          <form className='task-form' onSubmit={createTask}>
+               <input type='text' placeholder='Add a task' name={name} onChange={handleInputChange} value={name} />
+               
+               <button type='submit'>add</button>
+          </form>
      )
 }
 
--- frontend/src/components/task-list.js
+++ frontend/src/components/task-list.js
@@ -1,7 +1,21 @@
+import Task from "./task"
+import TaskForm from "./task-form"
+
 const taskList = () => {
      return (
           <div>
-               <h2>task</h2>
+               <h2>Task Manager</h2>
+               <TaskForm />
+               <div className="--flex-between --pd">
+                    <p>
+                         <b>Totle Task:</b> 0
+                    </p>
+                    <p>
+                         <b>Completed Task:</b> 0
+                    </p>
+               </div>
+               <hr />
+               <Task />
           </div>
      )
 }
