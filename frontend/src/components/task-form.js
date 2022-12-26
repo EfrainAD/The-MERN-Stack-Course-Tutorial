2d85aa1a484f6085631c0f4229e23cc76837d628
EfrainAD <105945226+EfrainAD@users.noreply.github.com>
2022-12-25 20:35:15 -0500

    Added the function to add task to the DB

--- frontend/src/components/task-form.js
+++ frontend/src/components/task-form.js
@@ -3,7 +3,7 @@ import React from 'react'
 const TaskForm = ({createTask, name, handleInputChange}) => {
      return (
           <form className='task-form' onSubmit={createTask}>
-               <input type='text' placeholder='Add a task' name={name} onChange={handleInputChange} value={name} />
+               <input type='text' placeholder='Add a task' name='name' onChange={handleInputChange} value={name} />
                
                <button type='submit'>add</button>
           </form>
--- frontend/src/components/task-list.js
+++ frontend/src/components/task-list.js
@@ -1,11 +1,32 @@
 import Task from "./task"
 import TaskForm from "./task-form"
+import { URL } from '../App'
+import { useState } from "react"
+import axios from 'axios'
+import { toast } from "react-toastify"
 
-const taskList = () => {
+const TaskList = () => {
+     const [taskData, setTaskData] = useState('')
+     const handleInputChange = (e) => {
+          const {name, value} = e.target
+          setTaskData({...taskData, [name]: value})
+     }
+     const createTask = async (e) => {
+          e.preventDefault()
+          if (taskData === '') return toast.error('Can not send an empty to-do')
+          try {
+               await axios.post(`${URL}/api/tasks`, taskData)
+               toast.success('Task has been added')
+               setTaskData('')
+          } catch (error) {
+               toast.error(error.message)
+          }
+     }
+     // name={name} onChange={} value={name} /
      return (
           <div>
                <h2>Task Manager</h2>
-               <TaskForm />
+               <TaskForm createTask={createTask} handleInputChange={handleInputChange} />
                <div className="--flex-between --pd">
                     <p>
                          <b>Totle Task:</b> 0
@@ -20,4 +41,4 @@ const taskList = () => {
      )
 }
 
-export default taskList
+export default TaskList
